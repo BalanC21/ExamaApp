7c5ee6271f9f24e559053285c24247f90362bcd4
BalanC21 <cristianbalan2021@gmail.com>
2023-03-22 19:51:27 +0200

    Bit refactor --> divide some methods in small methods (One method do only one thing)

--- backend/src/main/java/com/codecool/jpasecurity/service/ChartService.java
+++ backend/src/main/java/com/codecool/jpasecurity/service/ChartService.java
@@ -31,12 +31,6 @@ public class ChartService {
         this.getUsers = getUsers;
     }
 
-    private static long getSumByMonth(List<Expense> expenses, Month month) {
-        return expenses.stream().
-                filter(expense -> expense.getLocalDate()
-                        .getMonth().equals(month)).mapToLong(Expense::getAmount).sum();
-    }
-
     public List<CatChart> getPieChartDataForUser() {
         User owner = getUsers.getUser();
         return getCatChartsForUser(owner.userId);
@@ -62,17 +56,23 @@ public class ChartService {
         return toolTipDTOS;
     }
 
-    private List<Expense> getExpenseForOwner() {
-        User owner = getUsers.getUser();
-        return expenseRepository.findAllByOwner_UserId(owner.userId);
-    }
-
     public List<NeedlePieRevenueDTO> getNeedlePieRevenueDTO() {
         List<Expense> expenses = getAllExpensesByUser();
 
         long expensesSum = getExpensesAmountSum(expenses);
 
         return getNeedlePieRevenueDTOS(expensesSum);
+    }
+
+    private long getSumByMonth(List<Expense> expenses, Month month) {
+        return expenses.stream().
+                filter(expense -> expense.getLocalDate()
+                        .getMonth().equals(month)).mapToLong(Expense::getAmount).sum();
+    }
+
+    private List<Expense> getExpenseForOwner() {
+        User owner = getUsers.getUser();
+        return expenseRepository.findAllByOwner_UserId(owner.userId);
     }
 
     private List<CatChart> getCatChartsForUser(long userId) {
