8e0b69ed2ab2b5b6920c96ecc647cb159dd2df1f
BalanC21 <cristianbalan2021@gmail.com>
2023-03-22 19:56:54 +0200

    Toggle routing for getting expenses ordered by amounth

--- client/src/components/pages/ExpensePage/ExpensePage.jsx
+++ client/src/components/pages/ExpensePage/ExpensePage.jsx
@@ -7,20 +7,47 @@ import useSWR from "swr";
 import "../../../App.css";
 import ExpenseCatChart from "../../shared/ExpenseCatChart";
 import {ToastContainer} from "react-toastify";
+import {useAtom} from "jotai";
+import {ORDER_CRITERIA} from "../../StateManagement/Store";
 
 const ExpensePage = () => {
     const REACT_APP_CRUD_EXPENSES = process.env.REACT_APP_CRUD_EXPENSES
+    const REACT_APP_EXPENSES_ORDERED_BY_AMOUNT_ASC = process.env.REACT_APP_EXPENSES_ORDERED_BY_AMOUNT_ASC
+    const REACT_APP_EXPENSES_ORDERED_BY_AMOUNT_DESC = process.env.REACT_APP_EXPENSES_ORDERED_BY_AMOUNT_DESC
     const REACT_APP_CRUD_EXPENSES_CATEGORIES = process.env.REACT_APP_CRUD_EXPENSES_CATEGORIES
+    const [orderCriteria, setOrderCriteria] = useAtom(ORDER_CRITERIA);
     const tableHeaders = ["Date", "Description", "Category", "Amount"]
 
-    const {data: expenses, error: errorExpenses} =
-        useSWR(REACT_APP_CRUD_EXPENSES, newGetSWR, {revalidateOnFocus: false});
+    function getExpenses() {
+        switch (orderCriteria) {
+            case "asc":
+                return REACT_APP_EXPENSES_ORDERED_BY_AMOUNT_ASC
+            case "desc":
+                return REACT_APP_EXPENSES_ORDERED_BY_AMOUNT_DESC
+            default:
+                return REACT_APP_CRUD_EXPENSES
+
+        }
+    }
+
+    let {data: expenses, error: errorExpenses} =
+        useSWR(getExpenses(), newGetSWR, {revalidateOnFocus: false});
 
     const {data: expensesCategories, error: errorExpensesCategories} =
         useSWR(REACT_APP_CRUD_EXPENSES_CATEGORIES, newGetSWR, {revalidateOnFocus: false});
 
     if (errorExpensesCategories || errorExpenses) return <ErrorPage/>
     if (!expenses || !expensesCategories) return <div>Loading...</div>;
+
+    async function toggleExpenseOrderCriteria() {
+        if (orderCriteria === "asc") {
+            setOrderCriteria("desc")
+        } else if (orderCriteria === "desc") {
+            setOrderCriteria("asc")
+        } else {
+            setOrderCriteria("asc")
+        }
+    }
 
     return (
         <div className={"expense-container"}>
@@ -29,7 +56,8 @@ const ExpensePage = () => {
                 <table className="budget-tracker">
                     <thead>
                     <tr>
-                        {tableHeaders.map(element => <th key={element}>{element}</th>)}
+                        {tableHeaders.map(element => <th onClick={toggleExpenseOrderCriteria}
+                                                         key={element}>{element}</th>)}
                     </tr>
                     </thead>
                     <tbody className="entries">
